73917ac9fd40889a192ecb639766866ea3df267f
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-05-07 14:03:10 -0300

    feat: adding login to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { container } from "tsyringe";
 import { errorHandler } from "./middlewares/errorHandler";
+import { AuthenticationController } from "./modules/authentication/useCases/AuthenticationController";
 import { CreateUserController } from "./modules/users/useCases/createUser/CreateUserController";
 
 const routes = Router()
 
 const createUserController = container.resolve(CreateUserController);
+const authenticationController = container.resolve(AuthenticationController)
 
 routes.post("/user", createUserController.handle)
+routes.post("/login", authenticationController.handle)
 routes.use(errorHandler)
 
 export { routes }
